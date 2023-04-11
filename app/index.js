34fc428839d4df66f19c1eb952b49175ff6e615a
unknown <alan.a.n.r.ar@gmail.com>
2023-04-11 14:52:38 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hi! This is the home</Text>
+        </View>
+    )
+}
+
+export default Home;
